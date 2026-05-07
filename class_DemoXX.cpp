13431be2593b380fb081180dfce3f6f1d0faff81
Niranjankhatavkar <niranjankhatavkar07@gmail.com>
2026-05-07 20:54:47 +0530

    Add files via upload

---
+++ class_DemoXX.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Demo
+{
+
+public:
+    int i;      //Characteristics
+    float f;    //Characteristics
+
+    void fun()  //Behaviour
+    {
+        cout << "Inside Fun\n";
+    }
+};
+
+int main()
+{
+    Demo dobj;
+    cout<<dobj.i<<"\n";
+    dobj.fun();
+
+    return 0;
+}
